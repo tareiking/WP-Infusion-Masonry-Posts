6760628d3a273794b2281662f61e29249bc79d05
Tarei King <tareiking@gmail.com>
2014-07-25 23:20:50 +1000

    Add logic to detect whether masonry element exists in DOM

--- js/widget.js
+++ js/widget.js
@@ -1,15 +1,16 @@
-(function ($) {
-	"use strict";
-	$(function () {
-  var container = document.querySelector('#masonry-loop');
-    //create empty var msnry
-    var msnry;
-    // initialize Masonry after all images have loaded
-    imagesLoaded( container, function() {
-        msnry = new Masonry( container, {
-            itemSelector: '.masonry-entry',
-        });
+(function($) {
+    "use strict";
+    $(function() {
+        var container = document.querySelector('#masonry-loop');
+        //create empty var msnry
+        var msnry;
+        if ( container ) {
+            // initialize Masonry after all images have loaded
+            imagesLoaded(container, function() {
+            msnry = new Masonry(container, {
+                itemSelector: '.masonry-entry',
+            });
+            });
+        };
     });
-
-	});
 }(jQuery));
